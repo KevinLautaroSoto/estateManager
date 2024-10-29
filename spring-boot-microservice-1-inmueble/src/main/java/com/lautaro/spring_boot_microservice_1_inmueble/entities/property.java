4d24270ee6e8873ed1f11c40f087b68105b46fb2
LautaroUnlp <kevin.soto194341@alumnos.info.unlp.edu.ar>
2024-10-29 16:12:39 -0300

    creation complete of property entity

---
+++ spring-boot-microservice-1-inmueble/src/main/java/com/lautaro/spring_boot_microservice_1_inmueble/entities/property.java
@@ -0,0 +1,33 @@
+package com.lautaro.spring_boot_microservice_1_inmueble.entities;
+
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Table(name="inmueble")
+public class property {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name="name", length = 150, nullable = false)
+    private String name;
+
+    @Column(name="address", length = 500, nullable = false)
+    private String address;
+
+    @Column(value = name"picture", length = 1200, nullable = true)
+    private String picture;
+
+    @Column(name="price", nullable = false)
+    private Double price;
+
+    @Column(name="creation_date", nullable = false)
+    private LocalDateTime creationDate;
+
+
+}
