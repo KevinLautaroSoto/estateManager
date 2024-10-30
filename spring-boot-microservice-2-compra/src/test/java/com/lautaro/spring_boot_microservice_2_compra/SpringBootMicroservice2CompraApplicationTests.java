7591100767e4671dba1ed68c30dad4f35d728d60
LautaroUnlp <kevin.soto194341@alumnos.info.unlp.edu.ar>
2024-10-30 17:18:04 -0300

    add microservice to handle buys

---
+++ spring-boot-microservice-2-compra/src/test/java/com/lautaro/spring_boot_microservice_2_compra/SpringBootMicroservice2CompraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lautaro.spring_boot_microservice_2_compra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMicroservice2CompraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
