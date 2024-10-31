230625298502119a58bd93dd57c9e5b3434cbcf4
LautaroUnlp <kevin.soto194341@alumnos.info.unlp.edu.ar>
2024-10-31 18:08:49 -0300

    add new microservice api-gateway and complete the configuration

---
+++ spring-boot-microservice-3-api-gateway/src/main/java/com/lautaro/spring_boot_microservice_3_api_gateway/SpringBootMicroservice3ApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.lautaro.spring_boot_microservice_3_api_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMicroservice3ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMicroservice3ApiGatewayApplication.class, args);
+	}
+
+}
