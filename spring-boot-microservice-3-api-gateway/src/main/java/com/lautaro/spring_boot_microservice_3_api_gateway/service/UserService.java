3a00d6b996eaba514990fa82a90c6848fe9214db
LautaroUnlp <kevin.soto194341@alumnos.info.unlp.edu.ar>
2024-11-01 21:19:14 -0300

    complete userService

---
+++ spring-boot-microservice-3-api-gateway/src/main/java/com/lautaro/spring_boot_microservice_3_api_gateway/service/UserService.java
@@ -0,0 +1,14 @@
+package com.lautaro.spring_boot_microservice_3_api_gateway.service;
+
+import com.lautaro.spring_boot_microservice_3_api_gateway.entities.Role;
+import com.lautaro.spring_boot_microservice_3_api_gateway.entities.User;
+
+import java.util.Optional;
+
+public interface UserService {
+    User sabeUser(User user);
+
+    Optional<User> findByUsername(String username);
+
+    void chanageRole (Role newRole, String username);
+}
